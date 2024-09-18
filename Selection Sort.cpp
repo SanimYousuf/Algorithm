a54c561b9ff2228c89a96804844b4e433362567b
SanimYousuf <sanimyousuf02@gmail.com>
2024-09-18 18:08:59 +0600

    first commit with complexity analysis

---
+++ Selection Sort.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+    for(int pass=0; pass<n-1; pass++)
+    {
+        int smallest_idx = pass;
+
+        for(int i=pass+1; i<n; i++)
+        {
+            if(arr[i] < arr[smallest_idx])
+            {
+                smallest_idx = i;
+            }
+        }
+        int temp = arr[pass];
+        arr[pass] = arr[smallest_idx];
+        arr[smallest_idx] = temp;
+    }
+
+}
+
+int main()
+{
+    cout<<"Enter array size: "<<endl;
+    int n;
+    cin>>n;
+    int arr[n];
+
+    cout<<"Enter array elements: "<<endl;
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    selectionSort(arr,n);
+
+    cout<<"Sorted array: "<<endl;
+
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
